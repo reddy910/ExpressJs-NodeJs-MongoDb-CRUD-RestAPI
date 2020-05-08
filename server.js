7506b33217e31b2252bf20c563dc9bd3f74a6175
reddy910 <reddy910@outlook.com>
2020-05-07 23:49:36 -0400

    Update server.js
    added swaggerDocumentation

--- server.js
+++ server.js
@@ -3,8 +3,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const swaggerDefinition = {
+  info: {
+    title: 'Basic Nodejs-ExpressJs-CRUD-REST-API operations',
+    version: '1.0.0',
+    description: 'A basic node js project to perform CURD operations.',
+  },
+  host: `localhost:3000`,
+  basePath: '/',
+};
 
+const swaggerOptions = {
+  swaggerDefinition: swaggerDefinition, 
+  apis: [
+    './routes/*.js'
+  ], 
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use('/users', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const port = process.env.PORT||3000;
+
+//Routes
+/**
+ * @swagger
+ * /subscriber:
+ * get:
+ * description: Get all users
+ * responses:
+ * '200':
+ * description:Success response
+ */
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
 const db = mongoose.connection
@@ -16,4 +49,6 @@ app.use(express.json())
 const subscriberRouter = require('./routes/subscriber')
 app.use('/subscriber',subscriberRouter)
 
-app.listen(3000,()=>console.log('server started'))
+app.listen(port,()=>{
+  console.log('server started on port:',port)
+})
